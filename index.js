e83ac9d86702b8a53e738b8171f79e7e8c6d1d0d
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-03-10 13:57:58 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// function createBreedList(breedList) {
+//     //test hey
+//     document.getElementById("breed").innerHTML = `
+//     <select onchange="loadByBreed(this.value)">
+//     <option>Choose a dog breed</option>
+//     ${Object.keys(breedList).map(function (breed) {
+//         return `<option> ${breed}</option>`
+//     }).join('')}
+//     </select>
+//     `
+// }
